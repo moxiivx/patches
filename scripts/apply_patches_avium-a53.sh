f91d780a99729ddbfede7b894ac5854a70ef2be8
moxiivx <moxiix@proton.me>
2025-12-05 21:52:38 +0100

    improve script & fix it
    why i haven't fix it before? idk
    
    Signed-off-by: moxiivx <moxiix@proton.me>

--- scripts/apply_patches_avium-a53.sh
+++ scripts/apply_patches_avium-a53.sh
@@ -9,8 +9,20 @@ patch2="$HOME/patches/patches/a53_avium/0002-del-sfc-nfs-from-file-contexts.patc
 patch3="$HOME/patches/patches/a53_avium/0003-change-dimens.patch"
 patch4="$HOME/patches/patches/a53_avium/0004-add-blur-flag.patch"
 patch5="$HOME/patches/patches/a53_avium/0005-patch-libepicoperator.c.patch"
-patch6="$HOME/patches/patches/a53_avium/0006-fix-libsec-ril-missing-lib.patchh"
+patch6="$HOME/patches/patches/a53_avium/0006-fix-libsec-ril-missing-lib.patch"
 patch7="$HOME/patches/patches/a53_avium/0007-hmm.patch"
+
+#------------------------------------------
+#             moved to dir patches
+#------------------------------------------
+
+patch1_a="$HOME/roms/avium/device/samsung/a53x/0001-add-avium-stuff.patch"
+patch2_a="$HOME/roms/avium/device/samsung/s5e8825-common/0002-del-sfc-nfs-from-file-contexts.patch"
+patch3_a="$HOME/roms/avium/device/samsung/a53x/0003-change-dimens.patch"
+patch4_a="$HOME/roms/avium/device/samsung/a53x/0004-add-blur-flag.patch"
+patch5_a="$HOME/roms/avium/device/samsung/s5e8825-common/0005-patch-libepicoperator.c.patch"
+patch6_a="$HOME/roms/avium/vendor/samsung/s5e8825-common/0006-fix-libsec-ril-missing-lib.patch"
+patch7_a="$HOME/roms/avium/device/samsung/s5e8825-common/0007-hmm.patch"
 
 #------------------------------------------
 #             dirs to copy to
@@ -40,20 +52,41 @@ cp "$patch7" "$s5e8825_common_dt"
 cd "$a53x_dt"
 
 echo "applying a53x patches.."
-git am "$patch1" || git am --abort
-git am "$patch3" || git am --abort
-git am "$patch4" || git am --abort
+git am "$patch1_a" || git am --abort
+git am "$patch3_a" || git am --abort
+git am "$patch4_a" || git am --abort
 
 cd "$s5e8825_common_dt"
 
 echo "applying s5e8825-common patches.."
-git am "$patch2" || git am --abort
-git am "$patch5" || git am --abort
-git am "$patch7" || git am --abort
+git am "$patch2_a" || git am --abort
+git am "$patch5_a" || git am --abort
+git am "$patch7_a" || git am --abort
 
 cd "$vendor_s5e8825_dt"
 
 echo "applying s5e8825-common vendor patches.."
-git am "$patch6" || git am --abort
+git am "$patch6_a" || git am --abort
+
+echo "remove applied patches.."
+
+cd "$a53x_dt"
+
+echo "removing a53x patches.."
+rm -rf "$patch1_a"
+rm -rf "$patch3_a"
+rm -rf "$patch4_a"
+
+cd "$s5e8825_common_dt"
+
+echo "removing s5e8825-common patches.."
+rm -rf "$patch2_a"
+rm -rf "$patch5_a"
+rm -rf "$patch7_a"
+
+cd "$vendor_s5e8825_dt"
+
+echo "removing s5e8825-common vendor patches.."
+rm -rf "$patch6_a"
 
 echo "done!"
