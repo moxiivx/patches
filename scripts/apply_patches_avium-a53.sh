2c05e541ac7d2e32b9ec24ad37f0d99b9ab301e8
moxiivx <moxiix@proton.me>
2026-01-01 02:14:22 +0100

    add new patch and add patch to script (patch desc: add inculde privkeys in a53x_patches)
    Signed-off-by: moxiivx <moxiix@proton.me>

--- scripts/apply_patches_avium-a53.sh
+++ scripts/apply_patches_avium-a53.sh
@@ -11,6 +11,7 @@ patch4="$HOME/patches/patches/a53_avium/a53_patches/0004-add-blur-flag.patch"
 patch5="$HOME/patches/patches/a53_avium/s5e8825-common_patches/0005-patch-libepicoperator.c.patch"
 patch6="$HOME/patches/patches/a53_avium/vendor_patches/0006-fix-libsec-ril-missing-lib.patch"
 patch7="$HOME/patches/patches/a53_avium/s5e8825-common_patches/0007-hmm.patch"
+patch8="$HOME/patches/patches/a53_avium/a53_patches/0008-add-includepriv-keys.patch"
 
 #------------------------------------------
 #             moved to dir patches
@@ -23,6 +24,7 @@ patch4_a="$HOME/roms/avium/device/samsung/a53x/0004-add-blur-flag.patch"
 patch5_a="$HOME/roms/avium/device/samsung/s5e8825-common/0005-patch-libepicoperator.c.patch"
 patch6_a="$HOME/roms/avium/vendor/samsung/s5e8825-common/0006-fix-libsec-ril-missing-lib.patch"
 patch7_a="$HOME/roms/avium/device/samsung/s5e8825-common/0007-hmm.patch"
+patch8_a="$HOME/roms/avium/device/samsung/a53x/0008-add-includepriv-keys.patch"
 
 #------------------------------------------
 #             dirs to copy to
@@ -51,6 +53,7 @@ cp "$patch4" "$a53x_dt"
 cp "$patch5" "$s5e8825_common_dt"
 cp "$patch6" "$vendor_s5e8825_dt"
 cp "$patch7" "$s5e8825_common_dt"
+cp "$patch8" "$a53x_dt"
 
 #-------------------------------------------
 #              apply patches
@@ -62,6 +65,7 @@ echo "applying a53x patches.."
 git am "$patch1_a" || git am --abort
 git am "$patch3_a" || git am --abort
 git am "$patch4_a" || git am --abort
+git am "$patch8_a" || git am --abort
 
 cd "$s5e8825_common_dt"
 
@@ -83,6 +87,7 @@ echo "removing a53x patches.."
 rm -rf "$patch1_a"
 rm -rf "$patch3_a"
 rm -rf "$patch4_a"
+rm -rf "$patch8_a"
 
 cd "$s5e8825_common_dt"
 
